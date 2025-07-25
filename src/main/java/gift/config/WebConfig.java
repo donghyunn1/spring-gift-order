325d1a4819f1be53d6e172540a0323bc7ddb6b77
양동현 <dongdong0516@naver.com>
2025-07-25 14:57:19 +0900

    feat: 에러 수정

--- src/main/java/gift/config/WebConfig.java
+++ src/main/java/gift/config/WebConfig.java
@@ -25,7 +25,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/**")
                 .excludePathPatterns(
                         "/api/members/register",
-                        "/api/members/login"
+                        "/api/members/login",
+                        "/api/products"
                 );
     }
 
