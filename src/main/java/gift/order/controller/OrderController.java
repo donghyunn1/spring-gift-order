711fa51a8cd3012ed192d021883e7382e49ea01b
양동현 <dongdong0516@naver.com>
2025-07-30 01:44:20 +0900

    feat: OrderController 구현

---
+++ src/main/java/gift/order/controller/OrderController.java
@@ -0,0 +1,39 @@
+package gift.order.controller;
+
+import gift.jwt.auth.LoginMember;
+import gift.member.model.Member;
+import gift.order.dto.OrderRequestDto;
+import gift.order.dto.OrderResponseDto;
+import gift.order.service.OrderService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(
+            @Valid @RequestBody OrderRequestDto requestDto,
+            @LoginMember Member member,
+            HttpServletRequest request) {
+
+        String kakaoAccessToken = (String) request.getSession().getAttribute("kakaoAccessToken");
+        OrderResponseDto orderResponse = orderService.createOrder(member.getId(), kakaoAccessToken,
+                requestDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderResponse);
+    }
+}
