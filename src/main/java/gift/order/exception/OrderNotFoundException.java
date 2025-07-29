844e5763fe2bcbf9fc0ece277c2311309605c284
양동현 <dongdong0516@naver.com>
2025-07-30 01:43:23 +0900

    feat: Order관련 예외처리 클래스 구현

---
+++ src/main/java/gift/order/exception/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package gift.order.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
