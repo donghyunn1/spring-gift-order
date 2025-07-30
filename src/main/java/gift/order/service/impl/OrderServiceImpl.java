c8e27fa1836184634b700b4a6105a312d3bfb9b9
양동현 <dongdong0516@naver.com>
2025-07-31 02:54:43 +0900

    feat: 필요없는 Transactional 제거

--- src/main/java/gift/order/service/impl/OrderServiceImpl.java
+++ src/main/java/gift/order/service/impl/OrderServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
